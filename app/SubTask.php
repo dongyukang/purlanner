1179dfa3e6adecd98e7648f52b7bec47053b6ef2
Dongyu Kang <dongyu@dongyukang.com>
2017-09-16 13:47:29 -0400

    update task on subtask

--- app/SubTask.php
+++ app/SubTask.php
@@ -20,4 +20,36 @@ class SubTask extends Model
     {
         return $this->belongsTo(\App\Task::class);
     }
+
+    /**
+     * Get course info from subtask id.
+     *
+     * @return string
+     */
+    public function getCourseFromSubTaskId($subtask_id)
+    {
+      $task_id = \App\SubTask::find($subtask_id)->task_id;
+
+      $course_id = \App\Task::find($task_id)->course_id;
+
+      $course_subject = \App\Course::find($course_id)->subject;
+
+      $course_number = \App\Course::find($course_id)->course_number;
+
+      return $course_subject . ' ' . $course_number;
+    }
+
+    /**
+     * Get task from subtask id.
+     *
+     * @return string
+     */
+    public function getTaskFromSubTaskId($subtask_id)
+    {
+      $task_id = \App\SubTask::find($subtask_id)->task_id;
+
+      $task = \App\Task::find($task_id)->title;
+
+      return $task;
+    }
 }
--- resources/views/subtask/subtask.blade.php
+++ resources/views/subtask/subtask.blade.php
@@ -29,41 +29,43 @@
         </div>
         <div class="panel-body">
           @todoExists
-          @foreach (auth()->user()->getDatesOfSubTasks() as $date)
-            <div class="row">
-              <div class="col-xs-2">
-                <div class="panel panel-default">
-                  <div class="panel-heading">
-                    <center>
-                      <h4>{{ date('M', strtotime($date)) }}</h4>
-                    </center>
+            @foreach (auth()->user()->getDatesOfSubTasks() as $date)
+              <div class="row">
+                <div class="col-xs-2">
+                  <div class="panel panel-default">
+                    <div class="panel-heading">
+                      <center>
+                        <h4>{{ date('M', strtotime($date)) }}</h4>
+                      </center>
+                    </div>
+                    <div class="panel-body">
+                      <center>
+                        <h4>{{ \Carbon\Carbon::parse($date)->day }} ({{ date('D', strtotime($date)) }})</h4>
+                      </center>
+                    </div>
                   </div>
-                  <div class="panel-body">
-                    <center>
-                      <h4>{{ \Carbon\Carbon::parse($date)->day }} ({{ date('D', strtotime($date)) }})</h4>
-                    </center>
+                </div>
+                <div class="col-xs-10">
+                  <div class="panel panel-primary">
+                    <div class="panel-body">
+                      <ul>
+                        @foreach (auth()->user()->subtasks()->get() as $subtask)
+                          @if ($date == $subtask->due_date)
+                            <li>
+                              {{ $subtask->task }}
+                              <span class="label label-primary">{{ $subtask->getTaskFromSubTaskId($subtask->id) }}</span>
+                              <span class="label label-danger">{{ $subtask->getCourseFromSubTaskId($subtask->id) }}</span>
+                            </li>
+                          @endif
+                        @endforeach
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div class="col-xs-10">
-                <div class="panel panel-primary">
-                  <div class="panel-body">
-                    <ul>
-                      @foreach (auth()->user()->subtasks()->get() as $subtask)
-                        @if ($date == $subtask->due_date)
-                          <li>
-                            {{ $subtask->task }}
-                          </li>
-                        @endif
-                      @endforeach
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <hr />
-          @endforeach
+              <hr />
+            @endforeach
           @else
             <center> <h4> Nothing To Display </h4> </center>
           @endif
